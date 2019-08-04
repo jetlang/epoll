956e07b4ed27563ab07fbbb18fcbfcaf6dd3c6be
mrettig <mike.rettig@gmail.com>
2019-08-04 17:12:26 -0500

    add

--- src/test/java/org/jetlang/epoll/Main.java
+++ src/test/java/org/jetlang/epoll/Main.java
@@ -1,9 +1,14 @@
 package org.jetlang.epoll;
 
+import java.io.IOException;
+import java.nio.channels.DatagramChannel;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         EPoll e = new EPoll("epoll", 5, 16, 1024 * 8);
-
+        DatagramChannel channel = DatagramChannel.open();
+        channel.configureBlocking(false);
+        channel.close();
     }
 }
