2145f2a16a638e108d36012cd478a19c7872792d
Mike Rettig <mrettig@drw.com>
2019-08-07 15:29:41 -0500

    simplify

--- latency.sh
+++ latency.sh
@@ -2,4 +2,4 @@
 export LD_LIBRARY_PATH=.
 CP=out/production/epoll:out/test/epoll
 
-$JAVA_HOME/bin/java -cp $CP org.jetlang.epoll.LatencyMain $*
+exec $JAVA_HOME/bin/java -cp $CP org.jetlang.epoll.LatencyMain $*
